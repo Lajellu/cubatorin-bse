6e05d41d7b9d5d83d16e6cf371495198958bfbab
JS Booklet <jsbooklet@gmail.com>
2024-07-12 15:22:42 -0400

    Added Flask required packages

--- backend.py
+++ backend.py
@@ -390,4 +390,4 @@ def research():
 
 if __name__ == "__main__":
     print("Starting Flask server...")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
--- cubatorin/cubatorin/settingsprod.py
+++ cubatorin/cubatorin/settingsprod.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# 'django-insecure-x2!ss+eivq**^45u37_*9jva=__0(-+eiyd6pb0-=vygot05vq'
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
