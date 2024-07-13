7cbc45a15038b23848ae00fe70878661e8ae193f
JS Booklet <jsbooklet@gmail.com>
2024-07-13 10:54:15 -0400

    Turn on debuggin on prod settings. Getting CSRF token errors.

--- cubatorin/cubatorin/settingsprod.py
+++ cubatorin/cubatorin/settingsprod.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['137.184.166.117', 'cubatorin.com']
 
